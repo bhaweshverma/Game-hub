2e6976491923b8edd37e8243017b1d353d064111
Bhawesh Verma <65503795+bhaweshverma@users.noreply.github.com>
2023-07-17 15:25:01 +0000

    Build responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <>
-  <Button colorScheme='teal' size='sm'>
-    Button
-  </Button>
-  </>;
+  return (
+    <>
+      <Grid templateAreas={{
+        base: `"nav" "main"`, //one column for mobiles
+        lg: `"nav nav" "aside main"` //for devices larger than 1024px
+      }}>
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above='lg'>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
