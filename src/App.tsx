8f1ba2df638505ad6835a92ce2f0de99ac5f7d83
Bhawesh Verma <65503795+bhaweshverma@users.noreply.github.com>
2023-07-18 06:14:31 +0000

    NavBar component added with HStack>Logo+Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <Grid templateAreas={{
-        base: `"nav" "main"`, //one column for mobiles
-        lg: `"nav nav" "aside main"` //for devices larger than 1024px
-      }}>
-        <GridItem area="nav" bg="coral">
-          Nav
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`, //one column for mobiles
+          lg: `"nav nav" "aside main"`, //for devices larger than 1024px
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar></NavBar>
         </GridItem>
-        <Show above='lg'>
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
         </Show>
         <GridItem area="main" bg="dodgerblue">
           Main
